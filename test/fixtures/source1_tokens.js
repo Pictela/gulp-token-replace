3635ab3a103cb329768f97e1badd2f97e5d7f168
Stephen Erisman <stephen.erisman@wonderlic.com>
2014-08-27 15:22:06 -0500

    add mocha test cases

---
+++ test/fixtures/source1_tokens.js
@@ -0,0 +1,4 @@
+module.exports = {
+  baseUrl: 'http://www.somewhere.com',
+  pageTitle: 'Hello World!'
+};
---
+++ test/fixtures/source3_tokens.js
@@ -0,0 +1,9 @@
+module.exports = {
+  root: {
+    name: 'This is the root name',
+    first: {
+      name: 'First\'s Name',
+      age: 99
+    }
+  }
+};
